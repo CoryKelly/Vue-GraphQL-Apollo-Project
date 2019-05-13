af039906dd84526779fd74d0f267014b5478d0c4
CoryKelly <cory@muzeo.co>
2019-05-13 18:25:05 -0500

    Vuex - carousel - Loading
    - Vuex store laoding graphql data
    - Setup handler to show on home page
    - Added loading

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,55 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+import { gql } from "apollo-boost";
+import { defaultClient as apolloClient } from "./main";
+import { stat } from "fs";
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    posts: [],
+    loading: false
   },
   mutations: {
-
+    setPosts: (state, payload) => {
+      state.posts = payload;
+    },
+    setLoading: (state, payload) => {
+      state.loading = payload;
+    }
   },
   actions: {
-
+    getPosts: ({ commit }) => {
+      commit("setLoading", true);
+      // use ApolloClient to fire getPosts query
+      apolloClient
+        .query({
+          query: gql`
+            query {
+              getPosts {
+                _id
+                title
+                imageUrl
+              }
+            }
+          `
+        })
+        .then(({ data }) => {
+          // Get data from actions to state via mutations
+          // commit passes data from actions along to mutation functions
+          commit("setPosts", data.getPosts);
+          commit("setLoading", false);
+        })
+        .catch(err => {
+          commit("setLoading", false);
+          console.error(err);
+        });
+    }
+  },
+  getters: {
+    posts: state => state.posts,
+    loading: state => state.loading
   }
-})
+});
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,34 +1,75 @@
 <template>
-  <v-container>
-    <h1>Home</h1>
-    <ul
-      v-for="post in getPosts"
-      :key="post._id"
-    >
-      <li>
-        {{post.title}}
-        {{post.imageUrl}}
-        {{post.description}}
-      </li>
-    </ul>
+  <v-container text-xs-center>
+    <v-layout row>
+      <v-dialog
+        v-model="loading"
+        persistent
+        fullscreen
+      >
+        <v-container fill-height>
+          <v-layout
+            row
+            justify-center
+            align-center
+          >
+            <v-progress-circular
+              indeterminate
+              :size="70"
+              :width="7"
+              color="secondary"
+            ></v-progress-circular>
+          </v-layout>
+        </v-container>
+      </v-dialog>
+    </v-layout>
+
+    <v-flex xs12>
+      <v-carousel
+        v-if="!loading && posts.length > 0"
+        v-bind="{ 'cycle': true }"
+        interval="3000"
+      >
+        <v-carousel-item
+          v-for="post in posts"
+          :key="post._id"
+          :src="post.imageUrl"
+        >
+          <h1 id="carousel__title">{{post.title}}</h1>
+        </v-carousel-item>
+      </v-carousel>
+    </v-flex>
   </v-container>
 </template>
 
+<style scoped>
+#carousel__title {
+  position: absolute;
+  background-color: rgb(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5px 5px 0 0;
+  padding-top: 0.3em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+}
+</style>
+
 <script>
 import { gql } from "apollo-boost";
+import { mapGetters } from "vuex";
 export default {
   name: "home",
-  apollo: {
-    getPosts: {
-      query: gql`
-        query {
-          getPosts {
-            title
-            imageUrl
-            description
-          }
-        }
-      `
+  created() {
+    this.handleGetCarouselPosts();
+  },
+  computed: {
+    ...mapGetters(["loading", "posts"])
+  },
+  methods: {
+    handleGetCarouselPosts() {
+      // reach out to Vuex store, fire action that gets posts for carousel
+      this.$store.dispatch("getPosts");
     }
   }
 };
